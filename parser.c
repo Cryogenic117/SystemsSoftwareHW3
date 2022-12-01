3fed5efe36c4af0c5644db750702c7318451824c
Brandon Knudson <bknudson117@gmail.com>
2022-12-01 17:03:42 -0500

    Implemented Parse function and Procedures. Also, added comments to existing functions

--- parser.c
+++ parser.c
@@ -1,97 +1,217 @@
+/* Written By Brandon Knudson and Tristan Hedrick
+ * COP3402 
+ * Fall 2022
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "compiler.h"
-
+// Global Array's w/ Indices
 lexeme *tokens;
 int token_index = 0;
 symbol *table;
 int table_index = 0;
 instruction *code;
 int code_index = 0;
-
+// Other Variables
 int error = 0;
 int level;
-
+// Given Functions
 void emit(int op, int l, int m);
 void add_symbol(int kind, char name[], int value, int level, int address);
 void mark();
 int multiple_declaration_check(char name[]);
 int find_symbol(char name[], int kind);
-
-void block();
-void declarations();
-void constants();
-void statement(); 
-
+// Additional Functions for each Non-Terminal Symbol
+void block(); 		// B.K.
+void declarations();// B.K.
+void constants(); 	// B.K.
+void statement(); 	// T.H.
+void procedures();	// B.K.
+// Print Functions
 void print_parser_error(int error_code, int case_code);
 void print_assembly_code();
 void print_symbol_table();
 
 instruction *parse(int code_flag, int table_flag, lexeme *list)
 {
-	// your code here
+	// Variable Initialization
+	tokens = list;
+	table = calloc(ARRAY_SIZE, sizeof(symbol));
+	code = calloc(ARRAY_SIZE, sizeof(instruction));
+	level = -1;
+	// Main Procedure Block Recursive Call
+	block();
+	// If no stopping error finish program parsing
+	if(error != -1) {
+		if(tokens[token_index].type != period) {
+			print_parser_error(1,0);
+			error = 1;
+		}
+		// Search generated assembly code for each CAL and check if m is set to address
+		for(int i = 0; i < code_index ; i++) {
+			if(code[i].op == CAL && code[i].m != -1)  { // If CALL procedure is defined in instruction and m is set to something
+				if(table[code[i].m].address == -1) { // If Symbol table doesn't have address
+					print_parser_error(21,0);
+					error = 1;
+				}
+				else { // If the procedure has an address, set the m for CALL to this location
+					code[i].m = table[code[i].m].address;
+				}
+			}
+		} // If there is code to end the program block add it to code
+		if(code[code_index].op != GEQ || code[code_index].op != LOD) {
+			emit(GEQ,0,3);
+		}
+		// Print if Flags are set
+		if(error == 0) {
+			if(code_flag != 0) {
+				print_assembly_code();
+			}
+			if(table_flag != 0) {
+				print_symbol_table();
+			}
+			free(table);
+		}
+		// If there is an error at the end set code to null
+		else {
+			free(table);
+			free(code);
+			code == NULL;
+		}
+		// will return code if no errors otherwise null
+		return code;
+	}
+	// If there is an error somewhere in the program block return null
+	free(table);
+	free(code);
+	return NULL;
+}
+
+// large if-else statement used in constants and procedures
+int special_error_check() {
+	int tempTypeNum = tokens[token_index].type;
+		// Non-stopping error
+		if (
+			tempTypeNum == 3|| tempTypeNum == 4|| tempTypeNum == 5|| 
+			tempTypeNum == 1|| tempTypeNum == 6|| tempTypeNum == 7|| 
+			tempTypeNum == 9|| tempTypeNum == 11|| tempTypeNum == 13||
+			tempTypeNum == 14|| tempTypeNum == 16|| tempTypeNum == 15|| 
+			tempTypeNum == 17|| tempTypeNum == 22
+		) {
+			error = 1;
+		}
+		// Stopping Error
+		else {
+			error = -1;
+		}
+}
+
+void procedures() {
+	char * temp;
+	// Check for identifier and if the name is valid
+	token_index++;
+	if(tokens[token_index].type == identifier) {
+		if(multiple_declaration_check(tokens[token_index].identifier_name) != -1) {
+			print_parser_error(3,0);
+			error =1;
+		}
+		// Save Identifier Name for Later
+		strcpy(temp,tokens[token_index].identifier_name);
+		token_index++;
+	}
+	else {
+		print_parser_error(2,3);
+		if(tokens[token_index].type != semicolon) {
+			error = -1;
+			return;
+		}
+		error = 1;
+		temp = NULL;
+	}
+	add_symbol(LOD, temp, 0, level, -1);
+	if(tokens[token_index].type == semicolon) {
+		token_index++;
+	}
+	// If semi colon is missing check for stopping or non stopping error
+	else {
+		print_parser_error(6,3);
+		special_error_check();
+	}
+	return;
 }
 
 void block() {
+	// Create New Level
 	level++;
 	declarations();
+	// If there is not a stoppping error run statement
 	if(error != -1) {
-		mark();
-		level--;
-	}
-	statement();
+		statement();
+		// If there is not a stoppping error from statement, mark all symbols at this level to be inaccessible
 		if(error != -1) {
-		mark();
-		level--;
+			mark();
+			level--;
+		}
 	}
 	return;
 }
 
 void declarations() {
-	int var_count = 0;
+	// Track Variables() calls for addressing
+	const ACTIVATION_RECORD_SIZE = 3;
+	int variable_calls = 0;
 	while(1) {
+		// No need to increment in loop as the function calls will increment
 		int temp_token_type = tokens[token_index].type;
-		if(temp_token_type != 3 || temp_token_type != 4 || temp_token_type != 5) {
-			emit(INC,9,var_count+3);
+		// If Token is not a declaration INC to create space for activation record
+		if(temp_token_type != keyword_const || temp_token_type != keyword_var|| temp_token_type != keyword_procedure) {
+			emit(INC,9,variable_calls + ACTIVATION_RECORD_SIZE);
 			return;
 		}
+		// If Token type is constant
 		if(temp_token_type == keyword_const) {
 			constants();
 			if(error == -1) {
 				return;
 			}
 		}
+		// Otherwise token type is variable/procedure
 		else {
+			// If token type is procedure
 			if(temp_token_type == keyword_procedure) {
 				procedures();
 				if(error == -1) {
 					return;
 				}
-				continue;
 			}
-			variables(var_count);
-			var_count++;
-			if(error == -1) {
-				return;
+			// Otherwise token type is variable
+			else {
+				variables(variable_calls++);
+				if(error == -1) {
+					return;
+				}
 			}
 		}
-		token_index++;
 	}
 }
 
 void constants() {
-	int error_value;
+	// Saves identifier name for add_symbol call token is incremented
 	char* temp;
+	// Saves number as add_symbol call token is incremented
 	int someNumber;
-
+	// Is number negative or positive
+	int isMinus = 0;
 	token_index++;
+	// Next token should be the constant's identifier
 	if(tokens[token_index].type == identifier) {
-		error_value = multiple_declaration_check(tokens[token_index].identifier_name);
-		if(error_value != -1) {
+		// If there are multiple declarations with same name
+		if(multiple_declaration_check(tokens[token_index].identifier_name) != -1) {
 			print_parser_error(3,0);
 			error = 1;
 		}
+		// Set temp to identifier name
 		strcpy(temp,tokens[token_index].identifier_name);
 		token_index++;
 	}
@@ -104,6 +224,7 @@ void constants() {
 		error = 1;
 		temp = NULL;
 	}
+	// Next token should be the assignment symbol
 	if(tokens[token_index].type == assignment_symbol) {
 		token_index++;
 	}
@@ -114,7 +235,9 @@ void constants() {
 			return;
 		}
 	}
+	// Next symbol should be a minus and a number or just a number
 	if(tokens[token_index].type == minus) {
+		isMinus = 1;
 		token_index++;
 	}
 	if(tokens[token_index].type == number) {
@@ -130,28 +253,19 @@ void constants() {
 		error = 1;
 		someNumber = 0;
 	}
-	if(tokens[token_index].type == minus) {
+	// Now we have number so apply minus sign if present
+	if(isMinus) {
 		someNumber *= -1;
 	}
-	add_symbol(1,temp,someNumber, level, 0);
+	// Add to symbol table
+	add_symbol(identifier,temp,someNumber, level, 0);
+	// check for semicolon
 	if(tokens[token_index].type == semicolon) {
 		token_index++;
 	}
 	else {
 		print_parser_error(6,1);
-		int tempTypeNum = tokens[token_index].type;
-		if (
-			tempTypeNum == 3|| tempTypeNum == 4|| tempTypeNum == 5|| 
-			tempTypeNum == 1|| tempTypeNum == 6|| tempTypeNum == 7|| 
-			tempTypeNum == 9|| tempTypeNum == 11|| tempTypeNum == 13||
-			tempTypeNum == 14|| tempTypeNum == 16|| tempTypeNum == 15|| 
-			tempTypeNum == 17|| tempTypeNum == 22
-		) {
-			error = 1;
-		}
-		else {
-			error = -1;
-		}
+		special_error_check();
 	}
 	return;
 }
